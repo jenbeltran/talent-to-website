2a03806f664eae47fbfbb514e0b892e14cdb68da
jenbeltran <francescajenbeltran@gmail.com>
2020-11-29 10:18:25 -0500

    added our team section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import OurStory from "../components/OurStory"
+import Team from "../components/Team"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <OurStory />
+    <Team />
   </Layout>
 )
 
