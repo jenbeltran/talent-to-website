a755bd7749019b70b84b384c2e3fd02526da6568
Jen Beltran <francescajenbeltran@gmail.com>
2021-01-09 12:45:52 -0500

    changed event titles

--- src/components/CardDescription/pastEventsData.js
+++ src/components/CardDescription/pastEventsData.js
@@ -5,14 +5,14 @@ const Events = [
   {
     photo: PhotoOne,
     alt: "TalentTO panelists at an event",
-    title: "TalentTO Event",
+    title: "TalentTO Panel Discussion",
     description:
       "We had a guest panel discussion from Talent Leaders across the GTA which include Martin Hauck - Head of Talent at CoinSquare, Rebekeh Koenig - Director of People Operations at Roadmunk, Ted Bagley - Head of TA at TopHat, and Lorena Perry - Sr. Talent Acquisition at PeopleToGo to discuss the impacts and effects of Covid-19 and the Talent Industry.",
   },
   {
     photo: PhotoTwo,
     alt: "TalentTO panelists at an event",
-    title: "TalentTO Event",
+    title: "TalentTO Online Podcast",
     description:
       "TalentTO is partnered up with Dean Kulaweera – a seasoned professional in the Recruitment Industry who has over 8 years of experience and is a speaker and live streamer! We are going to be on his podcast of “Conversations with a #TechRecruiter” to discuss some of the biggest challenges of recruiting during a pandemic and will be joined by TalentTO Co-founders James Phanyasaeng and Vyvy Truong. Join us by watching live on linkedin with Dean and us or click to watch on Youtube.",
     link: "https://www.youtube.com/watch?v=yUdAD-VNMDM&feature=youtu.be",
