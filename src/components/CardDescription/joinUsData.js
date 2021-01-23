35e98950c72a7c7e205c9e317af7e4131d12bf6f
Jen Beltran <francescajenbeltran@gmail.com>
2021-01-23 16:45:45 -0500

    added social icons and podcast section

--- src/components/CardDescription/joinUsData.js
+++ src/components/CardDescription/joinUsData.js
@@ -14,11 +14,11 @@ const WaysToJoin = [
   {
     photo: PhotoTwo,
     alt: "Attendees at a networking event",
-    title: "Become a Speaker for our Podcast and Panels",
+    title: "Become a Speaker for our Podcast",
     description:
       "A podcast about inspiring Talent Professionals, sharing their career journey, best practices and techniques. As a speaker, you will be able to gain recognition, share your experiences and achievements with our community. We are super excited to have you!",
-    descriptionTwo:
-      "We host panel events that cover a wide range of topics in recruitment strategies, techniques, and trends. Becoming a speaker is an exceptional opportunity to strengthen your reputation in the industry, and network with other renowned speakers. You will play a vital role in helping TalentTO community to thrive. We are so grateful to anyone who is interested to join!",
+    // descriptionTwo:
+    //   "We host panel events that cover a wide range of topics in recruitment strategies, techniques, and trends. Becoming a speaker is an exceptional opportunity to strengthen your reputation in the industry, and network with other renowned speakers. You will play a vital role in helping TalentTO community to thrive. We are so grateful to anyone who is interested to join!",
     typeform:
       "https://form.typeform.com/to/z2udGtF6?typeform-medium=embed-snippet",
   },
